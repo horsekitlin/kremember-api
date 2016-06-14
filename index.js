5fd7f1f9c09cb0b307f2d064d10a560e6ddd5167
horsekitlin <horsekit1982@gmail.com>
2016-06-14 12:02:31 +0800

    test hook3

--- index.js
+++ index.js
@@ -7,30 +7,14 @@ import app from './src';
 var debug = require('debug')('node_skeleton:server');
 var http = require('http');
 
-/**
- * Get port from environment and store in Express.
- */
-
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
-
 var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
